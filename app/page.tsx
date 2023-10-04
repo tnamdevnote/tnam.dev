8e6b95f64f7e7c668583c94aff36de7b4e5d0a76
Taek Been Nam <tnam@TNM1.hsd1.mi.comcast.net>
2023-10-04 10:34:59 -0400

    style: add link to document fragment

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,64 @@
+"use client";
+
+import { useRef } from "react";
+import Link from "next/link";
+
 export default function Home() {
+  const aboutRef = useRef<HTMLElement>(null);
+  const experienceRef = useRef<HTMLElement>(null);
+  const projectRef = useRef<HTMLElement>(null);
+
+  // const handleClick = () => {
+  //   aboutRef.current?.scrollIntoView({
+  //     behavior: "smooth",
+  //     block: "start",
+  //   });
+  // };
+
   return (
     <main className="mx-auto grid min-h-screen max-w-screen-lg grid-cols-12 gap-8 px-6 py-12 lg:px-20 lg:py-0">
       <header className="col-span-full lg:sticky lg:top-0 lg:col-span-6 lg:max-h-screen lg:gap-8 lg:py-24">
-        <div>
-          <h1 className="text-h2 sm:text-h1">Taek B. Nam</h1>
-          <span className="text-h3 font-light">Frontend Developer</span>
+        <hgroup>
+          <h1 className="text-h2 sm:text-h1">
+            <Link href="#">Taek B. Nam</Link>
+          </h1>
+          <h3 className="text-h3 font-light">Frontend Developer</h3>
           <p className="mt-4 text-body text-accent-4">
             I enjoy creating polished user interfaces, but also deeply care
             about crafting products that enhance user experience.
           </p>
-        </div>
+        </hgroup>
         <nav className="hidden lg:mt-14 lg:block ">
           <ul className="flex flex-col gap-4">
             <li className="cursor-pointer">
-              <a href="#" className="group flex items-center">
+              <Link href="#top" className="group flex items-center">
                 <span className="mr-4 h-px w-8 bg-accent-3 transition-all group-hover:w-16 group-hover:bg-accent-6 group-focus-visible:w-16 group-focus-visible:bg-accent-6"></span>
-                <span className="text-accent-3 group-hover:text-accent-6 group-focus-visible:text-accent-6">
+                <span className="text-small text-accent-3 group-hover:text-accent-6 group-focus-visible:text-accent-6">
                   ABOUT
                 </span>
-              </a>
+              </Link>
             </li>
             <li className="cursor-pointer">
-              <a href="#" className="group flex items-center">
+              <Link href="#experience" className="group flex items-center">
                 <span className="mr-4 h-px w-8 bg-accent-3 transition-all group-hover:w-16 group-hover:bg-accent-6 group-focus-visible:w-16 group-focus-visible:bg-accent-6"></span>
-                <span className="text-accent-3 group-hover:text-accent-6 group-focus-visible:text-accent-6">
-                  EXPERIENCES
+                <span className="text-small text-accent-3 group-hover:text-accent-6 group-focus-visible:text-accent-6">
+                  EXPERIENCE
                 </span>
-              </a>
+              </Link>
             </li>
             <li className="cursor-pointer">
-              <a href="#" className="group flex items-center">
+              <Link href="#projects" className="group flex items-center">
                 <span className="mr-4 h-px w-8 bg-accent-3 transition-all group-hover:w-16 group-hover:bg-accent-6 group-focus-visible:w-16 group-focus-visible:bg-accent-6"></span>
-                <span className="text-accent-3 group-hover:text-accent-6 group-focus-visible:text-accent-6">
+                <span className="text-small text-accent-3 group-hover:text-accent-6 group-focus-visible:text-accent-6">
                   PROJECTS
                 </span>
-              </a>
+              </Link>
             </li>
           </ul>
         </nav>
       </header>
       <main className="col-span-full mt-16 lg:col-span-6 lg:m-0 lg:py-24">
-        <section className="mb-16">
+        <section id="about" className="mb-16" ref={aboutRef}>
           <h3 className="mb-8 text-body-bold lg:hidden">ABOUT</h3>
           <article className="text-body text-accent-4">
             Phasellus non ligula ut erat bibendum mollis. Aliquam accumsan
@@ -55,8 +73,8 @@ export default function Home() {
             tincidunt. Fusce id viverra mi, eget faucibus.
           </article>
         </section>
-        <section>
-          <h3 className="text-body-bold lg:hidden">EXPERIENCES</h3>
+        <section id="experience" ref={experienceRef}>
+          <h3 className="text-body-bold lg:hidden">EXPERIENCE</h3>
           <ol>
             <li className="mt-8">
               <header className="text-xs text-accent-4">
