f241e24d0ea78a16dec37143be7708d8ec91b75b
Taek Been Nam <taekbeen93@gmail.com>
2023-12-07 14:24:50 -0500

    style: apply dark mode - experience

--- app/components/card/card.tsx
+++ app/components/card/card.tsx
@@ -18,8 +18,12 @@ function Card({ children }: CardProps) {
 function Header({ title, subheader }: CardHeaderProps) {
   return (
     <div className="mb-2 flex items-center justify-between text-body">
-      <h3 className="text-body text-accent-8">{title}</h3>
-      <h5 className="text-xs text-accent-4">{subheader}</h5>
+      <h3 className="dark:text-dark-accent-8 text-body text-accent-8">
+        {title}
+      </h3>
+      <h5 className="dark:text-dark-accent-4 text-xs text-accent-4">
+        {subheader}
+      </h5>
     </div>
   );
 }
