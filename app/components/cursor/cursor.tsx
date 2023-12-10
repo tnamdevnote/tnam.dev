560f28875c4acf7b533f89522c7a1f4466fdc5d6
Taek Been Nam <taekbeen93@gmail.com>
2023-12-10 17:00:57 -0500

    chore: custom cursor adjustments

--- app/components/cursor/cursor.tsx
+++ app/components/cursor/cursor.tsx
@@ -2,18 +2,48 @@
 
 import { motion } from "framer-motion";
 import useMousePosition from "@/app/hooks/useMousePosition";
+import { useEffect, useRef } from "react";
 
 function Cursor() {
-  const { x, y } = useMousePosition();
+  // const { x, y } = useMousePosition();
+  const cursorRef = useRef<HTMLDivElement>(null);
+
+  const handleCursorMovement = (e: MouseEvent) => {
+    const { clientX, clientY } = e;
+    const cursor = cursorRef.current;
+    if (cursor) {
+      const mouseX = clientX - cursor.clientWidth / 2;
+      const mouseY = clientY - cursor.clientHeight / 2;
+      cursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousemove", (e) => handleCursorMovement(e));
+
+    const isTouchDevice =
+      "ontouchstart" in window ||
+      navigator.maxTouchPoints > 0 ||
+      navigator.maxTouchPoints > 0;
+
+    // Hide custom cursor on touch device
+    // if (isTouchDevice) {
+    //   console.log("This is a touch-enabled device (likely a mobile device)");
+    // } else {
+    //   console.log(
+    //     "This is not a touch-enabled device (likely not a mobile device)",
+    //   );
+    // }
+
+    return () =>
+      document.removeEventListener("mousemove", (e) => handleCursorMovement(e));
+  }, []);
 
   return (
-    <motion.div
+    <div
       className="pointer-events-none fixed z-50 h-4 w-4 -translate-x-10 -translate-y-10 rounded-full bg-teal-400 mix-blend-exclusion will-change-transform"
-      animate={{
-        x: x - 16 / 2,
-        y: y - 16 / 2,
-      }}
-    ></motion.div>
+      ref={cursorRef}
+    ></div>
   );
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 import { ThemeProvider } from "./context/themeProvider";
 
 export const metadata: Metadata = {
-  title: "Taek | Front End Developer",
+  title: "T. Nam | Front End Developer",
   description: "Welcome to tnamdevnote",
   icons: {
     icon: "/favicon.png",
